77707f7f3e880eb9a10a65be4a9049a04e151500
tomato-wu <h20333@qq.com>
2020-12-06 15:01:07 +0800

    2020/12/6

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import React from 'react';
 
 //App.js就是定义了一个react组件
 class App extends React.Component {
-  handleclick() {
-    alert('aaaaaaaaa')
+
+constructor(props){   //组件构造函数，就是App组件被创建的一瞬间就会执行
+  super(props);  //初始化不用管
+  this.state = {
+    list:[],
+    inputValue:''   //输入的值
+  }
+}
+  handleClick() {
+    this.setState({
+      list:[...this.state.list,this.state.inputValue],
+      inputValue:''   //inputValue输入框在点击add之后清空
+    })
   }
   render() {
     return (
@@ -11,12 +22,15 @@ class App extends React.Component {
       <div>
         <div>
           <div>
-            <input type="text" />
-            <button onClick={this.handleclick}>add</button>
+            <input/>
+            <button onClick={this.handleClick.bind(this)}>add</button>
           </div>
           <ul>
-            <li>chinese</li>
-            <li>english</li>
+            {
+              this.state.list.map((item,index)=>{
+              return <li key={index}>{item}</li>
+              })
+            }
           </ul>
         </div>
       </div>
@@ -24,4 +38,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default App;   //导出app组件
